ba19d8faed8afc34bea0ee465d03279589160957
AxRy <prajwalsingh7512@gmail.com>
2024-05-16 11:23:20 +0530

    Extra changes made for gallery made

--- client/src/components/ImageCarousel.js
+++ client/src/components/ImageCarousel.js
@@ -5,13 +5,16 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-import client1 from '../assests/client1.png'
-import client2 from '../assests/client2.png'
-import client3 from '../assests/client3.png'
-import client4 from '../assests/client4.png'
-import client5 from '../assests/client5.png'
-import client6 from '../assests/client6.png'
-import client7 from '../assests/client7.png'
+import client1 from '../assests/Gallery/gallery-1.jpg'
+import client2 from '../assests/Gallery/gallery-2.jpg'
+import client3 from '../assests/Gallery/gallery-3.jpg'
+import client4 from '../assests/Gallery/gallery-4.jpg'
+import client5 from '../assests/Gallery/gallery-5.jpg'
+import client6 from '../assests/Gallery/gallery-6.jpg'
+import client7 from '../assests/Gallery/gallery-7.jpg'
+import client8 from '../assests/Gallery/gallery-8.jpg'
+import client9 from '../assests/Gallery/gallery-9.jpg'
+import client10 from '../assests/Gallery/gallery-10.jpg'
 
 const responsive = {
     0: { 
@@ -29,7 +32,13 @@ const items = [
   <div className="item mx-4 h-60 flex justify-center items-center" data-value="1"><img src={client1} alt="Client 1" className='w-2/4' /></div>,
   <div className="item mx-4 h-60 flex justify-center items-center" data-value="2"><img src={client2} alt="Client 2" className='w-2/4' /></div>,
   <div className="item mx-4 h-60 flex justify-center items-center" data-value="3"><img src={client3} alt="Client 3" className='w-2/4' /></div>,
-  // Add other items similarly...
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="4"><img src={client4} alt="Client 4" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="5"><img src={client5} alt="Client 5" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="6"><img src={client6} alt="Client 6" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="7"><img src={client7} alt="Client 7" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="8"><img src={client8} alt="Client 8" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="9"><img src={client9} alt="Client 9" className='w-2/4' /></div>,
+  <div className="item mx-4 h-60 flex justify-center items-center" data-value="10"><img src={client10} alt="Client 10" className='w-2/4' /></div>,
 ];
 
 const ImageCarousel = () => {
